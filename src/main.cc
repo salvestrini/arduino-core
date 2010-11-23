ce0ce231f62ba6e0464647fcc903fa994d56bfc4
Francesco Salvestrini <salvestrini@gmail.com>
2010-11-23 22:44:05 +0100

    Rearranged main program

--- src/main.cc
+++ src/main.cc
@@ -4,11 +4,6 @@
 //avr-objcopy -O ihex -R .eeprom blink.out blink.hex
 //sudo avrdude -V -c dragon_isp -p m168 -b 19200 -P usb -U flash:w:cmaketest.hex
 
-//http://zedcode.blogspot.com/2007/02/gcc-c-link-problems-on-small-embedded.html
-//to keep from needing to link to c++ std lib, maybe? not totally sure
-// solves this error:
-
-//../../libarduino/libarduinocore.a(Print.o):(.data+0x6): undefined reference to `__cxa_pure_virtual'
 extern "C" void __cxa_pure_virtual(void)
 {
 	for(;;) {
@@ -21,15 +16,15 @@ int main()
 	init();
 
 	Serial.begin(9600);
-	pinMode(13, OUTPUT);
 
-	int pin = HIGH; 
+	pinMode(13, OUTPUT);
 
 	for(;;) {
 		Serial.println("Hello");
-		digitalWrite(13, pin);
-		(pin == HIGH) ? (pin = LOW) : (pin = HIGH);
+		digitalWrite(13, HIGH);
 		delay(1000);
+		digitalWrite(13, LOW);
 	}
+
 	return 0;
 }
