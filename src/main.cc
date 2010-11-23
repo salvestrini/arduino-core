c34328af21ce681a29662a130a6d9155128036e0
Francesco Salvestrini <salvestrini@gmail.com>
2010-11-23 22:50:44 +0100

    Removed useless main() (already in libs)

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,5 @@
 #include "WProgram.h"
 
-//after build -- to flash
-//avr-objcopy -O ihex -R .eeprom blink.out blink.hex
-//sudo avrdude -V -c dragon_isp -p m168 -b 19200 -P usb -U flash:w:cmaketest.hex
-
 extern "C" void __cxa_pure_virtual(void)
 {
 	for(;;) {
@@ -11,20 +7,18 @@ extern "C" void __cxa_pure_virtual(void)
 	}
 }
 
-int main()
+void setup()
 {
-	init();
-
 	Serial.begin(9600);
 
 	pinMode(13, OUTPUT);
 
-	for(;;) {
-		Serial.println("Hello");
-		digitalWrite(13, HIGH);
-		delay(1000);
-		digitalWrite(13, LOW);
-	}
+}
 
-	return 0;
+void loop()
+{
+        Serial.println("Hello");
+        digitalWrite(13, HIGH);
+        delay(1000);
+        digitalWrite(13, LOW);
 }
