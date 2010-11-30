308a7f08edbdec394b4f052940145e38bac254c1
Francesco Salvestrini <salvestrini@gmail.com>
2010-11-30 23:48:34 +0100

    Fixed compilation issues

--- cmake/arduino.cmake
+++ cmake/arduino.cmake
@@ -1,21 +1,17 @@
 ## http://www.paraview.org/Wiki/CMake_Cross_Compiling
-#
-#message(STATUS "Including ${CMAKE_CURRENT_LIST_FILE}")
-#
-#set(ARDUINO_TYPE diecimila CACHE STRING "Arduino type")
-#
-#include(arduino-${ARDUINO_TYPE})
-#
-#set(C_FLAGS_STANDARD   "-std=gnu99")
-#set(C_FLAGS_WARN       "-Wall -Wstrict-prototypes")
-#set(C_FLAGS_TUNING     "-funsigned-char -funsigned-bitfields -fpack-struct -fshort-enums")
-#set(C_FLAGS_OPT        "-Os")
-#
-#set(CXX_FLAGS_STANDARD "-std=gnu++98")
-#set(CXX_FLAGS_WARN     "-Wall")
-#set(CXX_FLAGS_TUNING   "")
-#set(CXX_FLAGS_OPT      "-Os")
-#
-#set(CMAKE_C_FLAGS          "${CMAKE_C_FLAGS}   ${ARDUINO_MCU} ${ARDUINO_DEFS} ${ARDUINO_C_FLAGS_OPT}   ${ARDUINO_C_FLAGS_STANDARD}   ${ARDUINO_C_FLAGS_WARN}   ${ARDUINO_C_FLAGS_TUNING}  ")
-#set(CMAKE_CXX_FLAGS        "${CMAKE_CXX_FLAGS} ${ARDUINO_MCU} ${ARDUINO_DEFS} ${ARDUINO_CXX_FLAGS_OPT} ${ARDUINO_CXX_FLAGS_STANDARD} ${ARDUINO_CXX_FLAGS_WARN} ${ARDUINO_CXX_FLAGS_TUNING}")
-#set(CMAKE_EXE_LINKER_FLAGS "")
+
+message(STATUS "Including ${CMAKE_CURRENT_LIST_FILE}")
+
+set(ARDUINO_TYPE diecimila CACHE STRING "Arduino type")
+
+include(arduino-${ARDUINO_TYPE})
+
+set(ARDUINO_C_FLAGS_STANDARD   "-std=gnu99")
+set(ARDUINO_C_FLAGS_WARN       "-Wall -Wstrict-prototypes")
+set(ARDUINO_C_FLAGS_TUNING     "-funsigned-char -funsigned-bitfields -fpack-struct -fshort-enums")
+set(ARDUINO_C_FLAGS_OPT        "-Os")
+
+set(ARDUINO_CXX_FLAGS_STANDARD "-std=gnu++98")
+set(ARDUINO_CXX_FLAGS_WARN     "-Wall")
+set(ARDUINO_CXX_FLAGS_TUNING   "")
+set(ARDUINO_CXX_FLAGS_OPT      "-Os")
