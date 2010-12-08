ca0030ed48b5946f66b7ad0fabd790c0785aca4a
Francesco Salvestrini <salvestrini@gmail.com>
2010-12-08 17:10:11 +0100

    Added SdFatLib example

--- src/main.cc
+++ src/main.cc
@@ -21,7 +21,7 @@
 #define PIN_LCD     2
 #define PIN_LED    13
 
-#define TEST        5
+#define TEST        6
 
 #include <WProgram.h>
 
@@ -301,6 +301,75 @@ void loop()
 }
 #endif
 
-#if TEST > 5
+#if TEST == 6
+#include <SdFat.h>
+#include <SdFatUtil.h>
+
+Sd2Card  card;
+SdVolume volume;
+SdFile   root;
+SdFile   file;
+
+// store error strings in flash to save RAM
+#define error(s) error_P(PSTR(s))
+
+void error_P(const char* str) {
+  PgmPrint("error: ");
+  SerialPrintln_P(str);
+  if (card.errorCode()) {
+    PgmPrint("SD error: ");
+    Serial.print(card.errorCode(), HEX);
+    Serial.print(',');
+    Serial.println(card.errorData(), HEX);
+  }
+  while(1);
+}
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println("type any character to start");
+  while (!Serial.available());
+  Serial.println();
+
+  // initialize the SD card at SPI_HALF_SPEED to avoid bus errors with
+  // breadboards.  use SPI_FULL_SPEED for better performance.
+  if (!card.init(SPI_HALF_SPEED)) error("card.init failed");
+
+  // initialize a FAT volume
+  if (!volume.init(&card)) error("volume.init failed");
+
+  // open the root directory
+  if (!root.openRoot(&volume)) error("openRoot failed");
+
+  // open a file
+  if (file.open(&root, "PRINT00.TXT", O_READ)) {
+    Serial.println("Opened PRINT00.TXT");
+  }
+  else if (file.open(&root, "WRITE00.TXT", O_READ)) {
+    Serial.println("Opened WRITE00.TXT");
+  }
+  else{
+    error("file.open failed");
+  }
+  Serial.println();
+
+  // copy file to serial port
+  int16_t n;
+  uint8_t buf[7];// nothing special about 7, just a lucky number.
+  while ((n = file.read(buf, sizeof(buf))) > 0) {
+    for (uint8_t i = 0; i < n; i++) Serial.print(buf[i]);
+  }
+  /* easier way
+  int16_t c;
+  while ((c = file.read()) > 0) Serial.print((char)c);
+  */
+  Serial.println("\nDone");
+}
+
+void loop(void) {}
+#endif
+
+#if TEST > 6
 #error Undefined test
 #endif
