f3d6dfec6e39f4b33ecf2f9170b7b4961d809554
Francesco Salvestrini <salvestrini@gmail.com>
2010-12-09 00:27:10 +0100

    Added examples, fixed building rules

--- src/main.cc
+++ src/main.cc
@@ -22,7 +22,7 @@
 #define PIN_LED    13
 
 #define PANIC_LED_SUPPORT 1
-#define TEST              7
+#define TEST              8
 
 #include <WProgram.h>
 
@@ -211,10 +211,10 @@ void setup()
 
         Serial.print("Initializing SD card ...\n");
 
-//        if (sd_raw_init() == 0) {
-//                Serial.print("Cannot initialize SD card\n");
-//                panic();
-//        }
+        //        if (sd_raw_init() == 0) {
+        //                Serial.print("Cannot initialize SD card\n");
+        //                panic();
+        //        }
 }
 
 int i = 0;
@@ -227,50 +227,50 @@ void loop()
 
         delay(1000);
 
-//        struct sd_raw_info info;
-//
-//        if (sd_raw_get_info(&info)) {
-//                Serial.print("SD card information:\n");
-//                Serial.print("  Manufacturer = ");
-//                Serial.print(info.manufacturer);
-//                Serial.print("\n");
-//
-//                Serial.print("  OEM          = ");
-//                Serial.print(info.oem[2]);
-//                Serial.print(info.oem[1]);
-//                Serial.print(info.oem[0]);
-//                Serial.print("\n");
-//
-//                Serial.print("  Product      = ");
-//                Serial.print(info.product[5]);
-//                Serial.print(info.product[4]);
-//                Serial.print(info.product[3]);
-//                Serial.print(info.product[2]);
-//                Serial.print(info.product[1]);
-//                Serial.print(info.product[0]);
-//                Serial.print("\n");
-//
-//                Serial.print("  Manufactured = ");
-//                Serial.print(info.manufacturing_month);
-//                Serial.print("/");
-//                Serial.print(2000 + info.manufacturing_year);
-//                Serial.print("\n");
-//
-//                Serial.print("  Copied       = ");
-//                Serial.print(info.flag_copy ? "yes" : "no");
-//                Serial.print("\n");
-//
-//                Serial.print("  Write prot.  = ");
-//                Serial.print(info.flag_write_protect ? "yes" : "no");
-//                Serial.print("\n");
-//
-//                Serial.print("  Format       = ");
-//                Serial.print(info.format);
-//                Serial.print("\n");
-//
-//        } else {
-//                Serial.print("Cannot read card information\n");
-//        }
+        //        struct sd_raw_info info;
+        //
+        //        if (sd_raw_get_info(&info)) {
+        //                Serial.print("SD card information:\n");
+        //                Serial.print("  Manufacturer = ");
+        //                Serial.print(info.manufacturer);
+        //                Serial.print("\n");
+        //
+        //                Serial.print("  OEM          = ");
+        //                Serial.print(info.oem[2]);
+        //                Serial.print(info.oem[1]);
+        //                Serial.print(info.oem[0]);
+        //                Serial.print("\n");
+        //
+        //                Serial.print("  Product      = ");
+        //                Serial.print(info.product[5]);
+        //                Serial.print(info.product[4]);
+        //                Serial.print(info.product[3]);
+        //                Serial.print(info.product[2]);
+        //                Serial.print(info.product[1]);
+        //                Serial.print(info.product[0]);
+        //                Serial.print("\n");
+        //
+        //                Serial.print("  Manufactured = ");
+        //                Serial.print(info.manufacturing_month);
+        //                Serial.print("/");
+        //                Serial.print(2000 + info.manufacturing_year);
+        //                Serial.print("\n");
+        //
+        //                Serial.print("  Copied       = ");
+        //                Serial.print(info.flag_copy ? "yes" : "no");
+        //                Serial.print("\n");
+        //
+        //                Serial.print("  Write prot.  = ");
+        //                Serial.print(info.flag_write_protect ? "yes" : "no");
+        //                Serial.print("\n");
+        //
+        //                Serial.print("  Format       = ");
+        //                Serial.print(info.format);
+        //                Serial.print("\n");
+        //
+        //        } else {
+        //                Serial.print("Cannot read card information\n");
+        //        }
 }
 
 #endif
@@ -326,7 +326,7 @@ void error(const char * str) {
         Serial.print("error: ");
         Serial.println(str);
  
-       if (card.errorCode()) {
+        if (card.errorCode()) {
                 Serial.print("SD error: ");
                 Serial.print(card.errorCode(), HEX);
                 Serial.print(',');
@@ -353,35 +353,35 @@ void setup(void) {
         // initialize a FAT volume
         if (!volume.init(&card)) error("volume.init failed");
 
-//  // open the root directory
-//  if (!root.openRoot(&volume)) error("openRoot failed");
-//
-//  // open a file
-//  if (file.open(&root, "PRINT00.TXT", O_READ)) {
-//          //Serial.println("Opened PRINT00.TXT");
-//  }
-//  else if (file.open(&root, "WRITE00.TXT", O_READ)) {
-//          //Serial.println("Opened WRITE00.TXT");
-//  }
-//  else{
-//          //error("file.open failed");
-//  }
-//  //Serial.println();
-//
-//#if 0  
-//  // copy file to serial port
-//  int16_t n;
-//  uint8_t buf[7];// nothing special about 7, just a lucky number.
-//  while ((n = file.read(buf, sizeof(buf))) > 0) {
-//    for (uint8_t i = 0; i < n; i++) Serial.print(buf[i]);
-//  }
-//  /* easier way
-//  int16_t c;
-//  while ((c = file.read()) > 0) Serial.print((char)c);
-//  */
-//  Serial.println("\nDone");
-//#endif
-}
+        //  // open the root directory
+        //  if (!root.openRoot(&volume)) error("openRoot failed");
+        //
+        //  // open a file
+        //  if (file.open(&root, "PRINT00.TXT", O_READ)) {
+        //          //Serial.println("Opened PRINT00.TXT");
+        //  }
+        //  else if (file.open(&root, "WRITE00.TXT", O_READ)) {
+        //          //Serial.println("Opened WRITE00.TXT");
+        //  }
+        //  else{
+        //          //error("file.open failed");
+        //  }
+        //  //Serial.println();
+        //
+        //#if 0  
+        //  // copy file to serial port
+        //  int16_t n;
+        //  uint8_t buf[7];// nothing special about 7, just a lucky number.
+        //  while ((n = file.read(buf, sizeof(buf))) > 0) {
+        //    for (uint8_t i = 0; i < n; i++) Serial.print(buf[i]);
+        //  }
+        //  /* easier way
+        //  int16_t c;
+        //  while ((c = file.read()) > 0) Serial.print((char)c);
+        //  */
+        //  Serial.println("\nDone");
+        //#endif
+                }
 
 void loop(void)
 {
@@ -402,32 +402,32 @@ SdCard card;
 #define error(s) error_P(PSTR(s))
 
 void error_P(const char* str) {
-  PgmPrint("error: ");
-  SerialPrintln_P(str);
-  if (card.errorCode) {
-    PgmPrint("SD error: ");
-    Serial.println(card.errorCode, HEX);
-  }
-  while(1);
+        PgmPrint("error: ");
+        SerialPrintln_P(str);
+        if (card.errorCode) {
+                PgmPrint("SD error: ");
+                Serial.println(card.errorCode, HEX);
+        }
+        while(1);
 }
 
 void setup() {
-  Serial.begin(9600);
-  PgmPrintln("Type any character to start");
-  while (!Serial.available());
+        Serial.begin(9600);
+        PgmPrintln("Type any character to start");
+        while (!Serial.available());
   
-  PgmPrint("Free RAM: ");
-  Serial.println(FreeRam());  
+        PgmPrint("Free RAM: ");
+        Serial.println(FreeRam());  
  
-  if (!card.init()) error("card.init failed!");
+        if (!card.init()) error("card.init failed!");
   
-  if (!Fat16::init(&card)) error("Fat16::init failed!");
+        if (!Fat16::init(&card)) error("Fat16::init failed!");
   
-  Serial.println();
+        Serial.println();
   
-  PgmPrintln("Name          Modify Date/Time    Size");
+        PgmPrintln("Name          Modify Date/Time    Size");
   
-  Fat16::ls(LS_DATE | LS_SIZE);
+        Fat16::ls(LS_DATE | LS_SIZE);
 }
 
 void loop()
@@ -436,6 +436,180 @@ void loop()
 }
 #endif
 
-#if TEST > 7
+#if TEST == 8
+#include <byteordering.h>
+#include <fat.h>
+#include <fat_config.h>
+#include <partition.h>
+#include <partition_config.h>
+#include <sd-reader_config.h>
+#include <sd_raw.h>
+#include <sd_raw_config.h>
+
+//Define the pin numbers
+#define CS     8
+#define MOSI  11
+#define MISO  12
+#define SCK   13
+
+//This is the amount of data to be fetched from the SD card for each read.
+#define BUFFERSIZE 256
+
+unsigned char buffer[BUFFERSIZE];
+char          file_name[30];
+
+struct fat_dir_struct* dd;		//FAT16 directory
+struct fat_dir_entry_struct dir_entry;	//FAT16 directory entry (A.K.A. a file)
+
+struct fat_fs_struct* fs;		//FAT16 File System
+struct partition_struct* partition;	//FAT16 Partition
+
+struct fat_file_struct * file_handle;	//FAT16 File Handle
+
+uint8_t find_file_in_dir(struct fat_fs_struct* fs, struct fat_dir_struct* dd, const char* name, struct fat_dir_entry_struct* dir_entry)
+{
+	fat_reset_dir(dd);	//Make sure to start from the beginning of the directory!
+        while(fat_read_dir(dd, dir_entry))
+                {
+                        if(strcmp(dir_entry->long_name, name) == 0)
+                                {
+                                        //fat_reset_dir(dd);
+                                        return 1;
+                                }
+                }
+
+        return 0;
+}
+
+struct fat_file_struct* open_file_in_dir(struct fat_fs_struct* fs, struct fat_dir_struct* dd, const char* name)
+{
+        struct fat_dir_entry_struct file_entry;
+        if(!find_file_in_dir(fs, dd, name, &file_entry))
+                return 0;
+
+        return fat_open_file(fs, &file_entry);
+}
+
+char init_filesystem(void)
+{
+	//setup sd card slot 
+	if(!sd_raw_init())
+                {
+                        return 0;
+                }
+
+	//open first partition
+	partition = partition_open(sd_raw_read,
+                                   sd_raw_read_interval,
+#if SD_RAW_WRITE_SUPPORT
+                                   sd_raw_write,
+                                   sd_raw_write_interval,
+#else
+                                   0,
+                                   0,
+#endif
+                                   0
+                                   );
+
+	if(!partition)
+                {
+                        //If the partition did not open, assume the storage device
+                        //is a "superfloppy", i.e. has no MBR.
+                        partition = partition_open(sd_raw_read,
+                                                   sd_raw_read_interval,
+#if SD_RAW_WRITE_SUPPORT
+                                                   sd_raw_write,
+                                                   sd_raw_write_interval,
+#else
+                                                   0,
+                                                   0,
+#endif
+                                                   -1
+                                                   );
+                        if(!partition)
+                                {
+                                        return 0;
+                                }
+                }
+
+	//Open file system
+	fs = fat_open(partition);
+	if(!fs)
+                {
+                        return 0;
+                }
+
+	//Open root directory
+	fat_get_dir_entry_of_path(fs, "/", &dir_entry);
+	dd=fat_open_dir(fs, &dir_entry);
+	
+	if(!dd)
+                {
+                        return 0;
+                }
+	return 1;
+}
+
+char get_next_filename(struct fat_dir_struct* cur_dir, char * new_file)
+{
+        //'dir_entry' is a global variable of type directory_entry_struct
+
+        //Get the next file from the root directory
+        if(fat_read_dir(cur_dir, &dir_entry))
+                {
+                        sprintf(new_file, "%s", dir_entry.long_name);
+                        Serial.println((const char *)new_file);
+                        return 1;
+                }
+        //If another file isn't found, return 0
+        return 0;
+}
+
+void setup()
+{
+        //Set up the pins for the Serial communication
+        pinMode(0, INPUT);
+        pinMode(1, OUTPUT);
+        Serial.begin(9600);
+ 
+        //Set up the pins for the microSD shield
+        pinMode(CS, OUTPUT);
+        pinMode(MOSI, OUTPUT);
+        pinMode(MISO, INPUT);
+        pinMode(SCK, OUTPUT);
+        pinMode(10, OUTPUT);
+}
+
+void loop()
+{
+        int bytes_read=0; //Keeps track of how many bytes are read when accessing a file on the SD card.
+
+        init_filesystem();	//Initialize the FAT16 file system on the SD card.
+  
+        //Get the next file in the directory
+        while(get_next_filename(dd, file_name)){
+                //Open the file	
+                file_handle=open_file_in_dir(fs, dd, file_name);
+                //Read up to 512 bytes from the file
+                bytes_read = fat_read_file(file_handle, buffer, BUFFERSIZE);
+                //Print whatever we just got from the file
+                Serial.println((const char *)buffer);
+                //Keep reading from the file until we reach the end (nothing more is read from the file)
+                while(bytes_read > 0){
+                        //If there's more to be read from the file, go get it.
+                        bytes_read = fat_read_file(file_handle, buffer, BUFFERSIZE);
+                        //Print the contents that have been read
+                        Serial.println((const char *)buffer);
+                }
+                //Close the file before moving on to the next one.
+                fat_close_file(file_handle);
+                delay(1000);
+                Serial.println("Next File...");
+        }
+        while(1);
+}
+#endif
+
+#if TEST > 8
 #error Undefined test
 #endif
