a83f0aa5bdcf7dc027ee3773b00f40d436619f89
Francesco Salvestrini <salvestrini@gmail.com>
2010-12-10 00:33:19 +0100

    Added E2PROM class

---
+++ src/E2PROM/E2PROM.h
@@ -0,0 +1,35 @@
+//
+// E2PROM - An E2PROM library
+//
+// Copyright (c) 2010 Francesco Salvestrini.  All right reserved.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+#ifndef E2PROM_H
+#define E2PROM_H
+
+class E2PROMClass {
+ public:
+        uint8_t read(size_t address);
+        void    write(size_t address, uint8_t value);
+
+ private:
+        bool isAddressOK(size_t address);
+};
+
+extern E2PROMClass E2PROM;
+
+#endif
