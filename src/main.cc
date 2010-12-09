49c46cee9876a182c6fd302c22c4615a7b9090b9
Francesco Salvestrini <salvestrini@gmail.com>
2010-12-09 01:00:37 +0100

    Rearranged main program

--- src/main.cc
+++ src/main.cc
@@ -22,7 +22,7 @@
 #define PIN_LED    13
 
 #define PANIC_LED_SUPPORT 1
-#define TEST              8
+#define TEST              7
 
 #include <WProgram.h>
 
@@ -381,7 +381,7 @@ void setup(void) {
         //  */
         //  Serial.println("\nDone");
         //#endif
-                }
+}
 
 void loop(void)
 {
@@ -469,14 +469,12 @@ struct fat_file_struct * file_handle;	//FAT16 File Handle
 uint8_t find_file_in_dir(struct fat_fs_struct* fs, struct fat_dir_struct* dd, const char* name, struct fat_dir_entry_struct* dir_entry)
 {
 	fat_reset_dir(dd);	//Make sure to start from the beginning of the directory!
-        while(fat_read_dir(dd, dir_entry))
-                {
-                        if(strcmp(dir_entry->long_name, name) == 0)
-                                {
-                                        //fat_reset_dir(dd);
-                                        return 1;
-                                }
+        while(fat_read_dir(dd, dir_entry)) {
+                if (strcmp(dir_entry->long_name, name) == 0) {
+                        //fat_reset_dir(dd);
+                        return 1;
                 }
+        }
 
         return 0;
 }
@@ -484,7 +482,7 @@ uint8_t find_file_in_dir(struct fat_fs_struct* fs, struct fat_dir_struct* dd, co
 struct fat_file_struct* open_file_in_dir(struct fat_fs_struct* fs, struct fat_dir_struct* dd, const char* name)
 {
         struct fat_dir_entry_struct file_entry;
-        if(!find_file_in_dir(fs, dd, name, &file_entry))
+        if (!find_file_in_dir(fs, dd, name, &file_entry))
                 return 0;
 
         return fat_open_file(fs, &file_entry);
@@ -493,10 +491,9 @@ struct fat_file_struct* open_file_in_dir(struct fat_fs_struct* fs, struct fat_di
 char init_filesystem(void)
 {
 	//setup sd card slot 
-	if(!sd_raw_init())
-                {
-                        return 0;
-                }
+	if (!sd_raw_init()) {
+                return 0;
+        }
 
 	//open first partition
 	partition = partition_open(sd_raw_read,
@@ -511,42 +508,38 @@ char init_filesystem(void)
                                    0
                                    );
 
-	if(!partition)
-                {
-                        //If the partition did not open, assume the storage device
-                        //is a "superfloppy", i.e. has no MBR.
-                        partition = partition_open(sd_raw_read,
-                                                   sd_raw_read_interval,
+	if (!partition) {
+                //If the partition did not open, assume the storage device
+                //is a "superfloppy", i.e. has no MBR.
+                partition = partition_open(sd_raw_read,
+                                           sd_raw_read_interval,
 #if SD_RAW_WRITE_SUPPORT
-                                                   sd_raw_write,
-                                                   sd_raw_write_interval,
+                                           sd_raw_write,
+                                           sd_raw_write_interval,
 #else
-                                                   0,
-                                                   0,
+                                           0,
+                                           0,
 #endif
-                                                   -1
-                                                   );
-                        if(!partition)
-                                {
-                                        return 0;
-                                }
+                                           -1
+                                           );
+                if (!partition) {
+                        return 0;
                 }
+        }
 
 	//Open file system
 	fs = fat_open(partition);
-	if(!fs)
-                {
-                        return 0;
-                }
+	if (!fs) {
+                return 0;
+        }
 
 	//Open root directory
 	fat_get_dir_entry_of_path(fs, "/", &dir_entry);
-	dd=fat_open_dir(fs, &dir_entry);
+	dd = fat_open_dir(fs, &dir_entry);
 	
-	if(!dd)
-                {
-                        return 0;
-                }
+	if (!dd) {
+                return 0;
+        }
 	return 1;
 }
 
@@ -555,12 +548,11 @@ char get_next_filename(struct fat_dir_struct* cur_dir, char * new_file)
         //'dir_entry' is a global variable of type directory_entry_struct
 
         //Get the next file from the root directory
-        if(fat_read_dir(cur_dir, &dir_entry))
-                {
-                        sprintf(new_file, "%s", dir_entry.long_name);
-                        Serial.println((const char *)new_file);
-                        return 1;
-                }
+        if (fat_read_dir(cur_dir, &dir_entry)) {
+                sprintf(new_file, "%s", dir_entry.long_name);
+                Serial.println((const char *)new_file);
+                return 1;
+        }
         //If another file isn't found, return 0
         return 0;
 }
@@ -573,23 +565,23 @@ void setup()
         Serial.begin(9600);
  
         //Set up the pins for the microSD shield
-        pinMode(CS, OUTPUT);
+        pinMode(CS,   OUTPUT);
         pinMode(MOSI, OUTPUT);
         pinMode(MISO, INPUT);
-        pinMode(SCK, OUTPUT);
-        pinMode(10, OUTPUT);
+        pinMode(SCK,  OUTPUT);
+        pinMode(10,   OUTPUT);
 }
 
 void loop()
 {
-        int bytes_read=0; //Keeps track of how many bytes are read when accessing a file on the SD card.
+        int bytes_read = 0; //Keeps track of how many bytes are read when accessing a file on the SD card.
 
         init_filesystem();	//Initialize the FAT16 file system on the SD card.
   
         //Get the next file in the directory
         while(get_next_filename(dd, file_name)){
                 //Open the file	
-                file_handle=open_file_in_dir(fs, dd, file_name);
+                file_handle = open_file_in_dir(fs, dd, file_name);
                 //Read up to 512 bytes from the file
                 bytes_read = fat_read_file(file_handle, buffer, BUFFERSIZE);
                 //Print whatever we just got from the file
@@ -603,6 +595,7 @@ void loop()
                 }
                 //Close the file before moving on to the next one.
                 fat_close_file(file_handle);
+
                 delay(1000);
                 Serial.println("Next File...");
         }
