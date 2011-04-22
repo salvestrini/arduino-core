eba73efca8110ca79c378317f47974c80739e245
Francesco Salvestrini <salvestrini@gmail.com>
2011-04-23 00:16:11 +0200

    Rearranged directory structure

--- cmake/arduino-diecimila.cmake
+++
@@ -1,4 +0,0 @@
-message(STATUS "Including ${CMAKE_CURRENT_LIST_FILE}")
-
-set(ARDUINO_MCU  "-mmcu=atmega168")
-set(ARDUINO_DEFS "-DF_CPU=16000000")
--- cmake/arduino-duemilanove.cmake
+++
@@ -1,4 +0,0 @@
-message(STATUS "Including ${CMAKE_CURRENT_LIST_FILE}")
-
-set(ARDUINO_MCU  "-mmcu=atmega328p")
-set(ARDUINO_DEFS "-DF_CPU=16000000")
